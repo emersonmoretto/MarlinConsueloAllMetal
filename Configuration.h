2e5a8e8900fb0390eb3dad9d4fefa22ab6c0deb1
Emerson Moretto <emoretto@usp.br>
2017-12-21 16:46:22 -0200

    versao com SD Card OK

--- Configuration.h
+++ Configuration.h
@@ -1229,7 +1229,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-//#define SDSUPPORT
+#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
@@ -1238,7 +1238,7 @@
  * This may be required to resolve "volume init" errors.
  */
 //#define SPI_SPEED SPI_HALF_SPEED
-//#define SPI_SPEED SPI_QUARTER_SPEED
+#define SPI_SPEED SPI_QUARTER_SPEED
 //#define SPI_SPEED SPI_EIGHTH_SPEED
 
 /**
